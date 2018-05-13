12a9d9482e5e9a9af68c28345be2574621621b03
veena <vhalliyal@gmail.com>
2018-05-13 09:28:49 +0530

    first commit

---
+++ src/test/java/com/practice/jenkins/JunitTest/Test1.java
@@ -0,0 +1,13 @@
+package com.practice.jenkins.JunitTest;
+
+import org.junit.Test;
+
+public class Test1 {
+	@Test
+	public void firstTest(){
+		for(int i=0;i<9;i++)
+		System.out.println("junit test");
+	}
+
+	
+}
