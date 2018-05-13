61f2aa5df0199b54d83c792e5834b65a9a9072ea
VeenaHalliyal <39082053+VeenaHalliyal@users.noreply.github.com>
2018-05-13 11:19:21 +0530

    Update Test1.java

--- src/test/java/com/practice/jenkins/JunitTest/Test1.java
+++ src/test/java/com/practice/jenkins/JunitTest/Test1.java
@@ -3,6 +3,8 @@ package com.practice.jenkins.JunitTest;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 
 
 public class Test1 {
